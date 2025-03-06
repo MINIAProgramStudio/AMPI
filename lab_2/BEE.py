f09be1a3e162f6d159daa567440891eef2b63cf2
Mykhailo Holub <mishataube2005@gmail.com>
2025-03-06 23:54:31 +0200

    bee done

--- lab_2/BEE.py
+++ lab_2/BEE.py
@@ -5,25 +5,35 @@ from matplotlib import pyplot as plt
 from matplotlib import animation, cm
 """
 coef list
-delta: ,
-n_max: ,
-alpha: ,
-pop_size: ,
-l_s: ,
-l_es: ,
-z_e: ,
-z_o: ,
-dim: ,
-minmax:
+"delta": ,
+"n_max": ,
+"alpha": ,
+"pop_size": ,
+"l_s": ,
+"l_es": ,
+"z_e": ,
+"z_o": ,
+"dim": ,
+"pos_min": ,
+"pos_max": 
 """
 class BEEHive:
     def __init__(self, coef, func, seeking_min = False):
         self.c = coef
         self.func = func
         self.seeking_min = seeking_min
-        self.best_pos = [random()*(self.c["minmax"][d][1]-self.c["minmax"][d][0])+self.c["minmax"][d][0] for d in range(self.c["dim"])]
+        self.best_pos = [random()*(self.c["pos_max"][d]-self.c["pos_min"][d])+self.c["pos_min"][d] for d in range(self.c["dim"])]
         self.best_value = self.func(self.best_pos)
-        self.bees = [[random()*(self.c["minmax"][d][1]-self.c["minmax"][d][0])+self.c["minmax"][d][0] for d in range(self.c["dim"])]
+        self.bees = [[random()*(self.c["pos_max"][d]-self.c["pos_min"][d])+self.c["pos_min"][d] for d in range(self.c["dim"])]
+                     for _ in range(self.c["pop_size"])]
+        self.iterations = 0
+
+    def reset(self):
+        self.best_pos = [random() * (self.c["pos_max"][d] - self.c["pos_min"][d]) + self.c["pos_min"][d] for d in
+                         range(self.c["dim"])]
+        self.best_value = self.func(self.best_pos)
+        self.bees = [[random() * (self.c["pos_max"][d] - self.c["pos_min"][d]) + self.c["pos_min"][d] for d in
+                      range(self.c["dim"])]
                      for _ in range(self.c["pop_size"])]
         self.iterations = 0
 
@@ -45,26 +55,37 @@ class BEEHive:
                 z = self.c["z_o"]
             nu = self.c["n_max"]*self.c["alpha"]**self.iterations
 
-            x_z = np.array([
-                self.bees[l][d] + nu * self.c["delta"](self.c["minmax"][d][1] - self.c["minmax"][d][0]) * (-1 + 2*random())
+            x_z = [[
+                np.maximum(self.c["pos_min"], np.minimum(self.c["pos_max"],
+                self.bees[l][d] + nu * self.c["delta"]*(self.c["pos_max"][d] - self.c["pos_min"][d]) * (-1 + 2*random()))[0])[0]
                 for d in range(self.c["dim"])
-            ])
-            x_z = np.maximum(self.c["minmax"][:][0], np.minimum(self.c["minmax"][:][1], x_z))
+            ] for _ in range(z)]
             x_z.sort(key = self.func, reverse = not self.seeking_min)
-            z_best_pos = x_z[0]
-            if self.func(z_best_pos) > self.func(self.bees[l]) and not self.seeking_min:
-                self.bees[l] = z_best_pos
-            elif self.func(z_best_pos) < self.func(self.bees[l]) and self.seeking_min:
-                self.bees[l] = z_best_pos
+            if self.func(x_z[0]) > self.func(self.bees[l]) and not self.seeking_min:
+                self.bees[l] = x_z[0]
+            elif self.func(x_z[0]) < self.func(self.bees[l]) and self.seeking_min:
+                self.bees[l] = x_z[0]
 
         for l in range(self.c["l_s"], self.c["pop_size"]):
-            self.bees[l] = [random()*(self.c["minmax"][d][1]-self.c["minmax"][d][0])+self.c["minmax"][d][0] for d in range(self.c["dim"])]
+            self.bees[l] = [random()*(self.c["pos_max"][d]-self.c["pos_min"][d])+self.c["pos_min"][d] for d in range(self.c["dim"])]
 
-        def solve(itertaions = 100, progressbar = False):
-            iterator = range(itertaions)
-            if progressbar:
-                iterator = tqdm(iterator, desc = "BEE")
-            for _ in iterator:
-                self.iter()
-            self.bees.sort(key=self.func, reverse=not self.seeking_min)
-            return (self.func(self.bees[0]), self.bees[0])
+    def solve(self, itertaions = 100, progressbar = False):
+        iterator = range(itertaions)
+        if progressbar:
+            iterator = tqdm(iterator, desc = "BEE")
+        for _ in iterator:
+            self.iter()
+        self.bees.sort(key=self.func, reverse=not self.seeking_min)
+        return (self.func(self.bees[0]), self.bees[0])
+
+    def solve_stats(self, iterations = 100, progressbar = False):
+        output = []
+        iterator = range(iterations)
+        if progressbar:
+            iterator = tqdm(iterator, desc="BEE")
+        self.bees.sort(key=self.func, reverse=not self.seeking_min)
+        for _ in iterator:
+            output.append(self.func(self.bees[0]))
+            self.iter()
+        output.append(self.func(self.bees[0]))
+        return (self.bees[0], self.best_pos, output)
--- lab_2/main.py
+++ lab_2/main.py
@@ -2,7 +2,7 @@ import numpy as np
 from PSO import PSOSolver
 from matplotlib import pyplot as plt
 from tqdm import tqdm
-
+from BEE import BEEHive
 
 def test_mean(object, iterations, tests, desc = "test_mean"):
     output = []
@@ -19,20 +19,68 @@ def rastring(pos):
         output += pos[d]**2 - 10*np.cos(2*np.pi*pos[d])
     return output
 
+def bee_opt(pos):
+    _bee = BEEHive({
+        "delta": pos[0],
+        "n_max": pos[1],
+        "alpha": pos[2],
+        "pop_size": 50,
+        "l_s": int(pos[3]),
+        "l_es": 10,
+        "z_e": 5,
+        "z_o": 2,
+        "dim": 5,
+        "pos_min": np.array([-5.12] * 5),
+        "pos_max": np.array([5.12] * 5)
+    }, rastring, True)
+    result = _bee.solve(25)[0]
+    return result
+
+pso_for_bee = PSOSolver({
+"a1": 0.05,#acceleration number
+"a2": 0.1,#acceleration number
+"pop_size": 50,#population size
+"dim": 4,#dimensions
+"pos_min": np.array([0,0,0,10]),#vector of minimum positions
+"pos_max": np.array([1,1,1,41]),#vector of maximum positions
+"speed_min": np.array([-0.5]*3+ [5]),#vector of min speed
+"speed_max": np.array([0.5]*3 + [5]),#vector of max speed
+}, bee_opt, True)
+#print(pso_for_bee.solve(50, True))
+
+
 pso = PSOSolver({
 "a1": 2,#acceleration number
 "a2": 3,#acceleration number
-"pop_size": 50,#population size
-"dim": 5,#dimensions
-"pos_min": np.array([-5.12]*5),#vector of minimum positions
-"pos_max": np.array([5.12]*5),#vector of maximum positions
-"speed_min": np.array([-0.5]*5),#vector of min speed
-"speed_max": np.array([0.5]*5),#vector of max speed
+"pop_size": 25,#population size
+"dim": 2,#dimensions
+"pos_min": np.array([-5.12]*2),#vector of minimum positions
+"pos_max": np.array([5.12]*2),#vector of maximum positions
+"speed_min": np.array([-0.5]*2),#vector of min speed
+"speed_max": np.array([0.5]*2),#vector of max speed
 }, rastring, True)
 
 print(pso.anisolve())
 
-y = test_mean(pso, 1000, 100)
-x = range(len(y))
-plt.plot(x,y)
+bee = BEEHive({
+    "delta": 0.75,
+    "n_max": 0.34,
+    "alpha": 0.74,
+    "pop_size": 25,
+    "l_s": 20,
+    "l_es": 10,
+    "z_e": 5,
+    "z_o": 2,
+    "dim": 2,
+    "pos_min":  np.array([-5.12]*2),
+    "pos_max": np.array([5.12]*2)
+}, rastring, True)
+
+print(bee.solve(100))
+
+pso_y = test_mean(pso, 100, 100)
+bee_y = test_mean(bee, 100, 100)
+
+plt.plot(range(len(pso_y)),pso_y, "b")
+plt.plot(range(len(bee_y)), bee_y, "r")
 plt.show()
