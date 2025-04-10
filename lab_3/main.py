3223ca00047031914f944ea6a6d3ad9128b5a160
Mykhailo Holub <mishataube2005@gmail.com>
2025-04-10 15:01:33 +0300

    lab_done?

--- lab_3/main.py
+++ lab_3/main.py
@@ -180,7 +180,8 @@ def plot_target_511(tN, aN):
     ts = np.linspace(1, 3, tN)
     al = np.linspace(-3, 3, aN)
     h = ts[1] - ts[0]
-    f = []
+    f_1 = []
+    f_2 = []
     for a in al:
         z = [1]
         x = [a]
@@ -195,21 +196,24 @@ def plot_target_511(tN, aN):
             y.append(y[-1] + dy * h)
             z.append(z[-1] + dz * h)
             if abs(x[-1]) > 10 ** 9:
-                f.append(abs(x[-1] - 3))
+                f_1.append(abs(x[-1] - 3))
+                f_2.append((x[-1] - 3)**2)
                 all_good = False
                 break
         if all_good:
-            f.append(abs(x[-1] - 3))
-    plt.plot(al, f)
-    print(min(f))
+            f_1.append(abs(x[-1] - 3))
+            f_2.append((x[-1] - 3) ** 2)
+    plt.plot(al, f_1, label = "abs")
+    plt.plot(al, f_2, label="sq")
     plt.yscale("log")
+    plt.legend()
     plt.show()
 
 
 plot_target_511(1000, 1000)
 
 
-def alpha_target_511(alpha):
+def alpha_target_511_abs(alpha):
     ts = np.linspace(1, 3, 1000)
     h = ts[1] - ts[0]
     z = [1]
@@ -226,6 +230,24 @@ def alpha_target_511(alpha):
         if abs(x[-1]) + abs(y[-1]) > 10000:
             return float("inf")
     return abs(x[-1] - 3)
+
+def alpha_target_511_sq(alpha):
+    ts = np.linspace(1, 3, 1000)
+    h = ts[1] - ts[0]
+    z = [1]
+    x = [alpha]
+    y = [1]
+    for i in range(1000):
+        dx = z[-1]
+        dy = y[-1] ** 4 + x[-1] ** 3 - 3 * np.sin(ts[i] * z[-1])
+        dz = (x[-1] ** 2) * (ts[i] ** 2) - (dy ** 2) * np.cos(z[-1])
+
+        x.append(x[-1] + dx * h)
+        y.append(y[-1] + dy * h)
+        z.append(z[-1] + dz * h)
+        if abs(x[-1]) + abs(y[-1]) > 10000:
+            return float("inf")
+    return (x[-1] - 3)**2
 
 def plot_alpha_target_511(tN, alpha):
     ts = np.linspace(1, 3, tN)
@@ -249,16 +271,16 @@ def plot_alpha_target_511(tN, alpha):
     plt.plot(ts, x[:-1])
     plt.show()
 
-cc_511 = CC({
+cc_511_abs = CC({
     "p_detect": 0.02,
     "delta": 0.25,
     "pop_size": 10,
     "dim": 1,
     "pos_min": [-3],
     "pos_max": [3]
-}, alpha_target_511, True)
+}, alpha_target_511_abs, True)
 
-bat_511 = BatSwarm({
+bat_511_abs = BatSwarm({
     "r0": 0.937,
     "A0": 1.169,
     "alpha": 0.95,
@@ -271,14 +293,44 @@ bat_511 = BatSwarm({
     "dim": 1,
     "pos_min": [-3],
     "pos_max": [3]
-}, alpha_target_511, True)
+}, alpha_target_511_abs, True)
+
+cc_511_sq = CC({
+    "p_detect": 0.02,
+    "delta": 0.25,
+    "pop_size": 10,
+    "dim": 1,
+    "pos_min": [-3],
+    "pos_max": [3]
+}, alpha_target_511_sq, True)
+
+bat_511_sq = BatSwarm({
+    "r0": 0.937,
+    "A0": 1.169,
+    "alpha": 0.95,
+    "delta": 0.62,
+    "gamma": 0.065,
+    "freq_min": 2.37,
+    "freq_max": 89.7,
+
+    "pop_size": 5,
+    "dim": 1,
+    "pos_min": [-3],
+    "pos_max": [3]
+}, alpha_target_511_sq, True)
 
 
 
-y_cc = test_mean(cc_511, 100, 5, "511_cc")
-y_bat = test_mean(bat_511, 100, 5, "511_bat")
-plt.plot(range(len(y_cc)),y_cc)
-plt.plot(range(len(y_bat)),y_bat)
+y_cc_abs = test_mean(cc_511_abs, 100, 5, "511_cc_abs")
+y_bat_abs = test_mean(bat_511_abs, 100, 5, "511_bat_abs")
+y_cc_sq = np.sqrt(test_mean(cc_511_sq, 100, 5, "511_cc_sq"))
+y_bat_sq = np.sqrt(test_mean(bat_511_sq, 100, 5, "511_bat_sq"))
+
+plt.plot(range(len(y_cc_abs)),y_cc_abs, label="CC_abs")
+plt.plot(range(len(y_bat_abs)),y_bat_abs, label="Bat_abs")
+plt.plot(range(len(y_cc_sq)),y_cc_sq, label="CC_sq")
+plt.plot(range(len(y_bat_sq)),y_bat_sq, label="Bat_sq")
+plt.legend()
 plt.show()
 
 def plot_512(tN, aN):
@@ -301,13 +353,14 @@ def plot_512(tN, aN):
                 break
             plt.plot(x, y)
     plt.show()
-plot_512(1000,10)
+plot_512(100,100)
 
 def plot_target_512(tN, aN):
     ts = np.linspace(1, 3, tN)
     al = np.linspace(-10, 10, aN)
     h = ts[1] - ts[0]
-    f = []
+    f_1 = []
+    f_2 = []
     for a in al:
         z = [a]
         x = [2]
@@ -322,21 +375,24 @@ def plot_target_512(tN, aN):
             y.append(y[-1] + dy * h)
             z.append(z[-1] + dz * h)
             if abs(x[-1]) > 10 ** 9:
-                f.append(abs(z[-1]))
+                f_1.append(abs(z[-1]))
+                f_2.append(z[-1]**2)
                 all_good = False
                 break
         if all_good:
-            f.append(abs(z[-1]))
-    plt.plot(al, f)
-    print(min(f))
+            f_1.append(abs(z[-1]))
+            f_2.append(z[-1] ** 2)
+    plt.plot(al, f_1, label="abs")
+    plt.plot(al, f_2, label="sq")
     plt.yscale("log")
+    plt.legend()
     plt.show()
 
 
 plot_target_512(1000, 10000)
 
 
-def alpha_target_512(alpha):
+def alpha_target_512_abs(alpha):
     ts = np.linspace(1, 3, 1000)
     h = ts[1] - ts[0]
     z = [alpha]
@@ -353,6 +409,24 @@ def alpha_target_512(alpha):
         if abs(x[-1]) + abs(y[-1]) > 10000:
             return float("inf")
     return abs(z[-1])
+
+def alpha_target_512_sq(alpha):
+    ts = np.linspace(1, 3, 1000)
+    h = ts[1] - ts[0]
+    z = [alpha]
+    x = [2]
+    y = [2]
+    for i in range(1000):
+        dx = 5 * x[-1] ** 2 - 25 * ts[i] ** 2 + (z[-1] ** 2) * np.cos(2 * ts[i] + x[-1])
+        dy = z[-1]
+        dz = 4 / (1 + (y[-1] ** 2) * (x[-1] ** 2)) + 4 * np.sin(ts[i] * dx)
+
+        x.append(x[-1] + dx * h)
+        y.append(y[-1] + dy * h)
+        z.append(z[-1] + dz * h)
+        if abs(x[-1]) + abs(y[-1]) > 10000:
+            return float("inf")
+    return (z[-1])**2
 
 def plot_alpha_target_512(tN, alpha):
     ts = np.linspace(1, 3, tN)
@@ -376,16 +450,16 @@ def plot_alpha_target_512(tN, alpha):
     plt.plot(ts, x[:-1])
     plt.show()
 
-cc_512 = CC({
+cc_512_abs = CC({
     "p_detect": 0.02,
     "delta": 0.25,
     "pop_size": 10,
     "dim": 1,
     "pos_min": [-10],
     "pos_max": [10]
-}, alpha_target_512, True)
+}, alpha_target_512_abs, True)
 
-bat_512 = BatSwarm({
+bat_512_abs = BatSwarm({
     "r0": 0.937,
     "A0": 1.169,
     "alpha": 0.95,
@@ -398,17 +472,46 @@ bat_512 = BatSwarm({
     "dim": 1,
     "pos_min": [-10],
     "pos_max": [10]
-}, alpha_target_512, True)
+}, alpha_target_512_abs, True)
+
+cc_512_sq = CC({
+    "p_detect": 0.02,
+    "delta": 0.25,
+    "pop_size": 10,
+    "dim": 1,
+    "pos_min": [-10],
+    "pos_max": [10]
+}, alpha_target_512_sq, True)
+
+bat_512_sq = BatSwarm({
+    "r0": 0.937,
+    "A0": 1.169,
+    "alpha": 0.95,
+    "delta": 0.62,
+    "gamma": 0.065,
+    "freq_min": 2.37,
+    "freq_max": 89.7,
+
+    "pop_size": 5,
+    "dim": 1,
+    "pos_min": [-10],
+    "pos_max": [10]
+}, alpha_target_512_sq, True)
 
 
+y_cc_abs = test_mean(cc_512_abs, 100, 5, "511_cc_abs")
+y_bat_abs = test_mean(bat_512_abs, 100, 5, "511_bat_abs")
+y_cc_sq = np.sqrt(test_mean(cc_512_sq, 100, 5, "511_cc_sq"))
+y_bat_sq = np.sqrt(test_mean(bat_512_sq, 100, 5, "511_bat_sq"))
 
-y_cc = test_mean(cc_512, 100, 5, "512_cc")
-y_bat = test_mean(bat_512, 100, 5, "512_bat")
-plt.plot(range(len(y_cc)),y_cc)
-plt.plot(range(len(y_bat)),y_bat)
+plt.plot(range(len(y_cc_abs)),y_cc_abs, label="CC_abs")
+plt.plot(range(len(y_bat_abs)),y_bat_abs, label="Bat_abs")
+plt.plot(range(len(y_cc_sq)),y_cc_sq, label="CC_sq")
+plt.plot(range(len(y_bat_sq)),y_bat_sq, label="Bat_sq")
+plt.legend()
 plt.show()
-
 """
+
 def plot_513(tN, aN):
     ts = np.linspace(1, 3, tN)
     al = np.linspace(-10, 10, aN)
@@ -429,13 +532,14 @@ def plot_513(tN, aN):
                 break
             plt.plot(x, y)
     plt.show()
-#plot_513(1000,100)
+plot_513(1000,100)
 
 def plot_target_513(tN, aN):
     ts = np.linspace(1, 3, tN)
     al = np.linspace(-10, 10, aN)
     h = ts[1] - ts[0]
-    f = []
+    f_1 = []
+    f_2 = []
     for a in al:
         z = [a]
         x = [2]
@@ -450,21 +554,24 @@ def plot_target_513(tN, aN):
             y.append(y[-1] + dy * h)
             z.append(z[-1] + dz * h)
             if abs(x[-1]) > 10 ** 9:
-                f.append(abs(z[-1]+1))
+                f_1.append(abs(z[-1]+1))
+                f_2.append((z[-1] + 1)**2)
                 all_good = False
                 break
         if all_good:
-            f.append(abs(z[-1]+1))
-    plt.plot(al, f)
-    print(min(f))
+            f_1.append(abs(z[-1]+1))
+            f_2.append((z[-1] + 1) ** 2)
+    plt.plot(al, f_1, label = "abs")
+    plt.plot(al, f_2, label="sq")
     plt.yscale("log")
+    plt.legend()
     plt.show()
 
 
-plot_target_513(1000, 10000)
+plot_target_513(1000, 1000)
 
 
-def alpha_target_513(alpha):
+def alpha_target_513_abs(alpha):
     ts = np.linspace(1, 3, 1000)
     h = ts[1] - ts[0]
     z = [alpha]
@@ -481,6 +588,24 @@ def alpha_target_513(alpha):
         if abs(x[-1]) + abs(y[-1]) > 10000:
             return float("inf")
     return abs(z[-1]+1)
+
+def alpha_target_513_sq(alpha):
+    ts = np.linspace(1, 3, 1000)
+    h = ts[1] - ts[0]
+    z = [alpha]
+    x = [2]
+    y = [1]
+    for i in range(1000):
+        dx = 2 * x[-1] ** 2 - 25 * ts[i] ** 2 - np.sin(x[-1] * y[-1] * ts[i])
+        dz = 1 - 4 * np.cos(dx * ts[i])
+        dy = z[-1]
+
+        x.append(x[-1] + dx * h)
+        y.append(y[-1] + dy * h)
+        z.append(z[-1] + dz * h)
+        if abs(x[-1]) + abs(y[-1]) > 10000:
+            return float("inf")
+    return (z[-1]+1)**2
 
 def plot_alpha_target_513(tN, alpha):
     ts = np.linspace(1, 3, tN)
@@ -504,16 +629,16 @@ def plot_alpha_target_513(tN, alpha):
     plt.plot(ts, x[:-1])
     plt.show()
 
-cc_513 = CC({
+cc_513_abs = CC({
     "p_detect": 0.02,
     "delta": 0.25,
     "pop_size": 10,
     "dim": 1,
     "pos_min": [-10],
     "pos_max": [10]
-}, alpha_target_513, True)
+}, alpha_target_513_abs, True)
 
-bat_513 = BatSwarm({
+bat_513_abs = BatSwarm({
     "r0": 0.937,
     "A0": 1.169,
     "alpha": 0.95,
@@ -526,12 +651,41 @@ bat_513 = BatSwarm({
     "dim": 1,
     "pos_min": [-10],
     "pos_max": [10]
-}, alpha_target_513, True)
+}, alpha_target_513_abs, True)
+
+cc_513_sq = CC({
+    "p_detect": 0.02,
+    "delta": 0.25,
+    "pop_size": 10,
+    "dim": 1,
+    "pos_min": [-10],
+    "pos_max": [10]
+}, alpha_target_513_sq, True)
+
+bat_513_sq = BatSwarm({
+    "r0": 0.937,
+    "A0": 1.169,
+    "alpha": 0.95,
+    "delta": 0.62,
+    "gamma": 0.065,
+    "freq_min": 2.37,
+    "freq_max": 89.7,
+
+    "pop_size": 5,
+    "dim": 1,
+    "pos_min": [-10],
+    "pos_max": [10]
+}, alpha_target_513_sq, True)
 
 
 
-y_cc = test_mean(cc_513, 100, 5, "512_cc")
-y_bat = test_mean(bat_513, 100, 5, "512_bat")
-plt.plot(range(len(y_cc)),y_cc)
-plt.plot(range(len(y_bat)),y_bat)
+y_cc_abs = test_mean(cc_513_abs, 100, 5, "513_cc_abs")
+y_bat_abs = test_mean(bat_513_abs, 100, 5, "513_bat_abs")
+y_cc_sq = test_mean(cc_513_sq, 100, 5, "513_cc_sq")
+y_bat_sq = test_mean(bat_513_sq, 100, 5, "513_bat_sq")
+plt.plot(range(len(y_cc_abs)),y_cc_abs, label = "cc_abs")
+plt.plot(range(len(y_bat_abs)),y_bat_abs, label = "bat_abs")
+plt.plot(range(len(y_cc_sq)),y_cc_sq, label = "cc_sq")
+plt.plot(range(len(y_bat_sq)),y_bat_sq, label = "bat_sq")
+plt.legend()
 plt.show()
